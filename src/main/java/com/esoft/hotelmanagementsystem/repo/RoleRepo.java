bda157e149c1eab3ee0b23f90b170a4c33bacca4
Shanil Irosh <shanil@misynergy.com>
2022-08-26 22:03:40 +0530

    test commit

--- src/main/java/com/esoft/hotelmanagementsystem/repo/RoleRepo.java
+++ src/main/java/com/esoft/hotelmanagementsystem/repo/RoleRepo.java
@@ -3,6 +3,7 @@ package com.esoft.hotelmanagementsystem.repo;
 import com.esoft.hotelmanagementsystem.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
