11b26183f78ac0647dcbe9a5f68424cef4fbfb73
Shanil Irosh <shanil@misynergy.com>
2022-08-28 13:24:29 +0530

    added swagger

--- src/main/java/com/esoft/hotelmanagementsystem/controller/TestController.java
+++ src/main/java/com/esoft/hotelmanagementsystem/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.esoft.hotelmanagementsystem.controller;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test1")
+    @ApiImplicitParams({ @ApiImplicitParam(name = "Authorization", required = true, dataType = "string", paramType = "header") })
     public String hello() {
         return "Received";
     }
