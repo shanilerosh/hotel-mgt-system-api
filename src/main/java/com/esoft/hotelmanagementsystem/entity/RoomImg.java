2dd955de1f9e7637667323feee0a861f228845dd
Shanil Irosh <shanil@misynergy.com>
2022-08-31 21:49:32 +0530

    added rooming tbl

---
+++ src/main/java/com/esoft/hotelmanagementsystem/entity/RoomImg.java
@@ -0,0 +1,30 @@
+package com.esoft.hotelmanagementsystem.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * @author ShanilErosh
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomImg {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomImgId;
+    @NotNull
+    private String roomPic;
+
+    @ManyToOne(targetEntity = RoomImg.class,fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_type_id", nullable = false)
+    private RoomType roomType;
+
+}
